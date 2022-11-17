c6ffb0e275cfe99df14b4ae930be640ca82dc8c2
dominik <dominikszczepka@gmail.com>
2022-11-17 23:37:51 +0100

    Password Hasher added

---
+++ PassSaver/PasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PassSaver
+{
+    public class PasswordHasher
+    {
+        public string Hash(string originalPassword)
+        {
+            var hashAlgorithm = SHA512.Create();
+            return Convert.ToBase64String(hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(originalPassword)));
+        }
+    }
+}
