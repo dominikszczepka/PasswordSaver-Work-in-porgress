a6950f68507a7c2bd3d109108ff64cee4071ee3c
dominik <dominikszczepka@gmail.com>
2022-11-17 23:38:59 +0100

    Checking if user exists

---
+++ PassSaver/Models/CheckIfUserExistsDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PassSaver.Models
+{
+    public class CheckIfUserExistsDto
+    {
+
+        [Required]
+        [MaxLength(64)]
+        public string Username { get; set; }
+        [Required]
+        public string UserUnhashedPassword { get; set; }
+    }
+}
